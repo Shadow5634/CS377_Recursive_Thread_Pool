50d0fb51374d129fb28d04fefbc50653596c7b9d
Shadow5634 <niharkkapasi@gmail.com>
2023-05-09 01:04:26 -0400

    set_up

--- ProjectThreads/include/recurMutex.h
+++ ProjectThreads/include/recurMutex.h
@@ -1,16 +1,23 @@
 #ifndef RECUR_MUTEX
 #define RECUR_MUTEX
 
-#include <stdlib.h>
-#include <string>
-#include <sys/wait.h>   /* for wait() */
-#include <stdlib.h>     /* for atoi() and exit() */
-#include <semaphore.h>  /* for sem */
-#include <assert.h>		/* for assert */
-#include <iostream>     /* for cout */
-#include <list>
-#include <array>
-#include <pthread.h>
-#include <unistd.h>
+  #include <stdlib.h>
+  #include <string>
+  #include <sys/wait.h>
+  #include <semaphore.h>
+  #include <assert.h>
+  #include <iostream>
+  #include <list>
+  #include <array>
+  #include <pthread.h>
+  #include <unistd.h>
+  #include <sys/mman.h>
+  #include <fstream>
+
+  // tries to lock a mutex using recursive lock approach
+  int lock(pthread_mutex_t* mutex);
+
+  // tries to unlock a mutex using recursive lock approach
+  int unlock(pthread_mutex_t* mutex);
 
 #endif 
--- ProjectThreads/src/recurMutex.cpp
+++ ProjectThreads/src/recurMutex.cpp
@@ -1,3 +1,9 @@
-#include <recurMutex.h>
+#include "../include/recurMutex.h"
 
 using namespace std;
+
+pthread_t currThreadID; // the thread that is currently holding the lock being trying to lock
+pthread_mutex_t tid_lock; // lock dealing with @currThreadID
+
+int count; // count for how many times the thread has acquired the lock
+pthread_mutex_t count_lock; // lock dealing with count
