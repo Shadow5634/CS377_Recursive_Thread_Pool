642e2fca52cbd78a96e61be5677c759d96806f67
Shadow5634 <niharkkapasi@gmail.com>
2023-05-09 01:32:21 -0400

    set_up

--- ProjectThreads/src/recurMutex.cpp
+++ ProjectThreads/src/recurMutex.cpp
@@ -10,13 +10,25 @@ int count; // count for how many times the thread has acquired the lock
 pthread_mutex_t count_lock; // lock dealing with count
 
 // makes a recursive lock that shall be used by recur_mutex_lock and recur_mutex_unlock (as parameters)
-int recur_mutex_init(pthread_mutex_t* mutex);
+int recur_mutex_init(pthread_mutex_t* mutex)
+{
+  return 0;
+}
 
 // destroys the created recursive lock
-int recur_mutex_destroy(pthread_mutex_t* mutex);
+int recur_mutex_destroy(pthread_mutex_t* mutex)
+{
+  return 0;
+}
 
 // tries to lock a mutex using recursive lock approach
-int recur_mutex_lock(pthread_mutex_t* mutex);
+int recur_mutex_lock(pthread_mutex_t* mutex)
+{
+  return 0;
+}
 
 // tries to unlock a mutex using recursive lock approach
-int recur_mutex_lock(pthread_mutex_t* mutex);
+int recur_mutex_lock(pthread_mutex_t* mutex)
+{
+  return 0;
+}
