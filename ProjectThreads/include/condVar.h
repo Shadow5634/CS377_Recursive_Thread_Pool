fb6ab60242ce246d027b9ba48c91d30dd7b3669f
Shadow5634 <niharkkapasi@gmail.com>
2023-05-11 14:30:37 -0400

    condVar file set-up

---
+++ ProjectThreads/include/condVar.h
@@ -0,0 +1,25 @@
+#ifndef COND_VAR
+#define COND_VAR
+
+  ////////////////////////////////////////
+  // PART 1 - Important Includes
+  ////////////////////////////////////////
+
+  #include <stdlib.h>
+  #include <string>
+  #include <sys/wait.h>
+  #include <semaphore.h>
+  #include <assert.h>
+  #include <iostream>
+  #include <list>
+  #include <array>
+  #include <pthread.h>
+  #include <unistd.h>
+  #include <sys/mman.h>
+  #include <fstream>
+
+  ////////////////////////////////////////
+  // PART 2 - Function TODOs
+  ////////////////////////////////////////
+
+#endif
