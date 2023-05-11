e530aae6874a0f1967ec5260cd6e926e743be855
Shadow5634 <niharkkapasi@gmail.com>
2023-05-11 12:50:02 -0400

    one phase

--- ProjectThreads/src/recurMutex.cpp
+++ ProjectThreads/src/recurMutex.cpp
@@ -16,13 +16,13 @@ using namespace std;
   // after it has the first time
   // for globals you create think about how multiple threads accessing affects it
 
-pthread_t currThreadID; // the thread that is currently holding the lock being trying to lock
+pthread_t currThreadID = 0; // the thread that is currently holding the lock being trying to lock
                         // 0 - no thread has the lock, it can be acquired, other values are actual tids 
-pthread_mutex_t tid_lock; // lock dealing with @currThreadID and @proc_id
-pthread_cond_t sleep_cond; // condition variable that deals with sleeping if lock is busy
+pthread_mutex_t tid_lock = PTHREAD_MUTEX_INITIALIZER; // lock dealing with @currThreadID and @proc_id
+pthread_cond_t sleep_cond = PTHREAD_COND_INITIALIZER; // condition variable that deals with sleeping if lock is busy
 
-int count; // count for how many times the thread has acquired the lock
-pthread_mutex_t count_lock; // lock dealing with count
+int count = 0; // count for how many times the thread has acquired the lock
+pthread_mutex_t count_lock = PTHREAD_MUTEX_INITIALIZER;; // lock dealing with count
 
 // ANSWER START
 
@@ -37,6 +37,8 @@ pthread_mutex_t count_lock; // lock dealing with count
 // in this case it acts as the mutex to be utilized for condition variables
 
 // it does not seem to be the case but think about whether these functions must be atomic
+
+// MIGHT WANT TO CHECK THAT INIT AND DESTROY CALLED ONLY ONCE
 // ANSWER END
 
 // makes a recursive lock that shall be used by recur_mutex_lock and recur_mutex_unlock (as parameters)
@@ -47,6 +49,7 @@ int recur_mutex_init(pthread_mutex_t* mutex)
   // ANSWER FOLLOWS:
   // =================================================================================
   // =================================================================================
+  pthread_mutex_init(mutex, NULL);
   return 0;
 }
 
@@ -58,6 +61,7 @@ int recur_mutex_destroy(pthread_mutex_t* mutex)
   // ANSWER FOLLOWS:
   // =================================================================================
   // =================================================================================
+  pthread_mutex_destroy(mutex);
   return 0;
 }
 
@@ -91,28 +95,25 @@ int recur_mutex_try_lock(pthread_mutex_t* mutex)
     // recursive acquisition of a free lock by same thread
     if (currThreadID == pthread_self())
     {
-      pthread_mutex_lock(&count_lock);
-        count++;
-      pthread_mutex_unlock(&count_lock);
-
       res = 0;
     }
     // recursive acquisition of a free lock by different thread 
     else if (currThreadID == 0)
     {
       currThreadID = pthread_self();
-      pthread_mutex_lock(&count_lock);
-        count = 1;
-      pthread_mutex_unlock(&count_lock);
-
       res = 1;
     }
     else
     {
-      res = -1;
+      pthread_mutex_unlock(&tid_lock);
+      return -1;
     }
 
   pthread_mutex_unlock(&tid_lock);
+
+  pthread_mutex_lock(&count_lock);
+    count++;
+  pthread_mutex_unlock(&count_lock);
 
   return res;
 }
@@ -141,30 +142,31 @@ int recur_mutex_unlock(pthread_mutex_t* mutex)
   int res;
 
   pthread_mutex_lock(&tid_lock);
-    // lock already free
+    // lock already free or trying to free a lock you have not acquired
     // will it be a good safety to have broadcast or signal here as well? 
-    if (currThreadID == 0)
+    if ((currThreadID == 0) || (currThreadID != pthread_self()))
     {
-      res = -1;
+      pthread_mutex_unlock(&tid_lock);
+      return -1;
     }
-    else
-    {
-      pthread_mutex_lock(&count_lock);
 
-        // recursive lock released all the way - it is now truly unlocked
-        if (--count = 0)
-        {
-          currThreadID = 0;
-          res = 1;
-        }
-        // recursive lock released one by level
-        else
-        {
-          res = 0;
-        }
+    // do you want to separate this with tid_lock 
+    pthread_mutex_lock(&count_lock);
 
-      pthread_mutex_unlock(&count_lock);
-    }
+      // recursive lock released all the way - it is now truly unlocked
+      if (--count = 0)
+      {
+        currThreadID = 0;
+        pthread_cond_broadcast(&sleep_cond); // signal should suffice, threads are all same if not acquired unlike malloc where diff threads asking for different memory
+        res = 1;
+      }
+      // recursive lock released one by level
+      else
+      {
+        res = 0;
+      }
+
+    pthread_mutex_unlock(&count_lock);
   pthread_mutex_unlock(&tid_lock);
 
   return res;
@@ -185,6 +187,26 @@ int recur_mutex_lock(pthread_mutex_t* mutex)
   // ANSWER FOLLOWS:
   // =================================================================================
   // =================================================================================
+
+  int res; // do you want to differentiate between reacquisition and first acquisition
+
+  pthread_mutex_lock(&tid_lock);
+    // lock has been acquired by a different thread   
+    // infinitely wait until you can assume control
+    while ((currThreadID != 0) && (currThreadID != pthread_self()))
+    {
+      // put to sleep
+      pthread_cond_wait(&sleep_cond, &tid_lock);
+    }
+
+    // assume control
+    currThreadID = pthread_self();
+  pthread_mutex_unlock(&tid_lock);
+
+  // you have been made the owner, increment count:
+  pthread_mutex_lock(&count_lock);
+    count++;
+  pthread_mutex_unlock(&count_lock);
 
   return 0;
 }
