d7d623e1d58465283d16acd19d756537c8fe1497
Shadow5634 <niharkkapasi@gmail.com>
2023-05-14 15:48:04 -0400

    cond var main test

--- ProjectThreads/src/main.cpp
+++ ProjectThreads/src/main.cpp
@@ -1,31 +1,53 @@
 #include "../include/recurMutex.h"
+#include "../include/condVar.h"
 
 using namespace std;
 
-int arr[2];
+pthread_t arr[2];
+pthread_mutex_t l;
+
+ConditionVariable* cv;
 
 int main(int argc, char** argv)
 {
   // declarations
   void basicRecurTest();
-  void condSigReceiver();
-  void condSigSender();
+  void* condSigReceiver(void* vargp);
+  void* condSigSender(void* vargp);
 
   // testing
 
-  // pthread_create();
+  pthread_mutex_init(&l, NULL);
+
+  cv = new ConditionVariable();
+
+  pthread_create(arr, NULL, condSigSender, NULL);
+  pthread_create((arr + 1), NULL, condSigReceiver, NULL);
+  pthread_join(arr[0], NULL);
+  pthread_join(arr[1], NULL);
 
   // basicRecurTest();
+
+  delete cv;
   return 0;
 }
 
 void* condSigSender(void* vargp)
 {
+  cout << "Sender starting\n" << endl;
+  sleep(5);
+  cout << "Sender sending broadcast\n" << endl;
+  cv->cond_var_broadcast();
   return NULL;
 }
 
 void* condSigReceiver(void* vargp)
 {
+  cout << "Receiver going to sleep\n" << endl;
+  pthread_mutex_lock(&l);
+    cv->cond_var_wait(&l);
+  pthread_mutex_unlock(&l);
+  cout << "Receiver awoken\n" << endl;
   return NULL;
 }
 
